e89d3adfaaacb364bf8c6ce2c2f03495dbfc357e
Tingting Zhao <zhaott0416@gmail.com>
2019-04-17 22:47:43 -0400

    add helper functions

--- bnpy/util/AnalyzeDP.py
+++ bnpy/util/AnalyzeDP.py
@@ -5,6 +5,7 @@ Created on Fri Apr 12 13:44:02 2019
 
 @author: crystal
 """
+import numpy as np
 
 ## extract DP parameters and organize them into a function
 def extractDPParam(model, dataset):
@@ -42,6 +43,52 @@ def extractDPParam(model, dataset):
     DPParam['nu'] = nu
     DPParam['kappa'] = kappa
     return DPParam
+
+def obtainObsInd4Cluster(Y, clusterLabel):
+    ind = np.where(Y == clusterLabel)[0]
+    return ind
+    
+
+def obtainObsInd4MultiCluster(Y, multiClusterLabel):
+    result = dict()
+    for value in multiClusterLabel:
+        result[value] = obtainObsInd4Cluster(Y, value)
+    return result
+
+def obtainTrueClusterLabel4FittedCluster(trueY, fittedY, fittedCluster):
+    """
+    This function returns the true cluster label for a fitted cluster given true 
+    labels and fitted cluster labels. For example, if in a fitted cluster, there 
+    are 10 data points, and 7 of them has comes from the true cluster with label
+    5, this cluster should match with the true cluster label 5. 
+    Args:
+        trueY: true cluster label for each observation
+        fittedY: the fitted cluster label for each observation
+        fittedCluster: the given fitted cluster Label
+    Returns:
+        the true cluster label correspond to the majority of the observations 
+        in the fitted cluster with label fittedCluster
+    """
+    ## this code has been tested
+    ## find the observations in the fitted cluster with cluster label fittedCluster
+    ind = obtainObsInd4Cluster(fittedY, fittedCluster)
+    trueClusters = trueY[ind]
+    uniqueTrueCluster, counts = np.unique(trueClusters, return_counts=True)
+    ## find the true cluster label with the largest number of counts by 
+    ## returning the index of counts with has the maximum value
+    majorityInd = np.argmax(counts)
+    ## obtain the proportion of the obs has the majority cluster label, 
+    ## which is also the precision
+    prec = counts[majorityInd]/len(ind)
+    majorityCluster = uniqueTrueCluster[majorityInd]
+    
+    result = dict()
+    result['prec'] = prec
+    result['cluster'] = majorityCluster
+    return result
+    
+
+
 
 def acc_single_number(trueY, fitteY, clusterTrue, clusterFitted):
     trueInd = np.where(trueY==clusterTrue)[0]
